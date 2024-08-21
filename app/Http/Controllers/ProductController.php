aacde4257aacca7db1b1a3cabaadb961d35b1504
ken-delantar <kenjayson83@gmail.com>
2024-08-21 21:11:12 +0800

    v1

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('products.index');
+    }
+
+    public function create(){
+
+        return view('products.create');
+    }
+}
